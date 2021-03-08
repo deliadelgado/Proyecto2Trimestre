7168dd54856ed25c5cf4cf8c83772be6f436091f
deliadelgado <deliadelgadodominguez@gmail.com>
2021-03-08 02:30:15 +0100

    first commit

---
+++ scr/consultas.js
@@ -0,0 +1,380 @@
+/*Horas a pagar por sexo*/
+db.alumnos.aggregate([
+    {$match: {$or:[{Sexo:'M'},{Sexo: 'F'}]}},
+    {$group: {_id: '$Sexo', HorassinCobrar: {$sum:'$Horas_aPagar'}}}
+    ]).pretty()
+    { 
+        "_id" : "F", 
+    "HorassinCobrar" : 271 }
+    { 
+        "_id" : "M", 
+        "HorassinCobrar" : 235 }
+    
+    
+    /*Horas a pagar por edad*/
+    
+    
+    db.alumnos.aggregate([
+        {$group: {_id: '$Edad', HorassinCobrar: {$sum: '$Horas_aPagar'}}},
+        {$sort:{_id : -1}}
+    ])
+
+    { 
+        "_id" : 19, 
+        "HorassinCobrar" : 35 }
+    { 
+        "_id" : 18, 
+        "HorassinCobrar" : 62 }
+    { 
+        "_id" : 17, 
+    "HorassinCobrar" : 75 }
+    { 
+        "_id" : 16, 
+    "HorassinCobrar" : 69 }
+    { 
+        "_id" : 15, 
+    "HorassinCobrar" : 81 }
+    { 
+        "_id" : 14, 
+    "HorassinCobrar" : 116 }
+    { 
+        "_id" : 13, 
+    "HorassinCobrar" : 68 }
+    
+    
+    /*horas totales que se le debe a la academia*/
+    db.alumnos.aggregate([
+        {$group: {_id: '$Edad', HorassinCobrar: {$sum: '$Horas_aPagar'}}},
+        {$sort:{_id : -1}},
+        {$group: {_id: '', Total_aDeber: {$sum: '$HorassinCobrar'} }}
+    ])
+    
+    { "_id" : "", "Total_aDeber" : 506 }
+    
+    /*  
+    Recuento de forma separada según sexo del curso en el que están los alumnos
+    */
+    db.alumnos.aggregate([
+    {$match:{$or: [ {Sexo: 'F'},{ Sexo: 'M'}]}},
+    {$group: {_id:'$Curso', NumAlumnos:{ $sum: 1}}},
+    {$sort: {Curso:1}}
+    ])
+
+    { 
+        "_id" : "3ESO", 
+        "NumAlumnos" : 5 }
+    { 
+        "_id" : "2BACH", 
+        "NumAlumnos" : 5 }
+    { 
+        "_id" : "1BACH", 
+        "NumAlumnos" : 5 }
+    { 
+        "_id" : "1ESO", 
+        "NumAlumnos" : 6 }
+    { 
+        "_id" : "4ESO", 
+        "NumAlumnos" : 4 }
+    { 
+        "_id" : "2ESO", 
+        "NumAlumnos" : 5 }
+    
+    
+   
+    /* Necesitamos saber cuantas horas se le debe a cada profesor,
+     sabiendo que el profesor cobra la hora a dos euros menos que la academia a los alumnos, 
+     cuanto dinero se le debe a cada profesor */
+    
+     /*HORAS*/ 
+    db.asignaturas.aggregate([
+        {$lookup:
+        {from:'alumnos',
+        localField: '_id',
+        foreignField: 'idas',
+        as: 'alumnos'}},
+        {$project:
+        {
+            _id: 0,
+            Asignatura: '$Materia',
+            Horasnopagadas: {$sum: '$alumnos.Horas_aPagar'}
+        }}
+    ])
+
+
+    { 
+        "Asignatura" : "Lengua", 
+        "Horasnopagadas" : 47 }
+    { 
+        "Asignatura" : "Química", 
+        "Horasnopagadas" : 55 }
+    {
+         "Asignatura" : "Inglés", 
+         "Horasnopagadas" : 62 }
+    {
+         "Asignatura" : "Matemáticas", 
+         "Horasnopagadas" : 45 }
+    {
+         "Asignatura" : "Biología",
+        "Horasnopagadas" : 57 }
+    { 
+        "Asignatura" : "Latín", 
+        "Horasnopagadas" : 50 }
+    {
+         "Asignatura" : "Tecnología", 
+         "Horasnopagadas" : 42 }
+    {
+         "Asignatura" : "Física", 
+         "Horasnopagadas" : 38 }
+    { 
+        "Asignatura" : "Griego", 
+        "Horasnopagadas" : 63 }
+    { 
+        "Asignatura" : "Francés", 
+        "Horasnopagadas" : 47 }
+    
+    /*DINERO */
+    
+    db.asignaturas.aggregate([
+        {$lookup:
+        {from:'alumnos',
+        localField: '_id',
+        foreignField: 'idas',
+        as: 'alumnos'}},
+        {$project: {
+        _id: 0,
+        Asignatura: '$Materia',
+        DineroAPagar: {$multiply: [ {$sum: ['$alumnos.Horas_aPagar']}, {$subtract: ['$PrecioHora', 2]}]}
+     }}
+    ])
+    {
+    "Asignatura" : "Griego",
+    "DineroaPagar" : 378 }
+    { 
+    "Asignatura" : "Inglés", 
+    "DineroaPagar" : 496 }
+    { 
+        "Asignatura" : "Biología", 
+        "DineroaPagar" : 342 }
+    { 
+        "Asignatura" : "Química", 
+        "DineroaPagar" : 330 }
+    { 
+         "Asignatura" : "Latín", 
+         "DineroaPagar" : 350 }
+    { 
+         "Asignatura" : "Lengua", 
+         "DineroaPagar" : 329 }
+    {
+         "Asignatura" : "Francés", 
+         "DineroaPagar" : 423}
+    {
+         "Asignatura" : "Matemáticas", 
+         "DineroaPagar" : 270}
+    {
+         "Asignatura" : "Tecnología", 
+         "DineroaPagar" : 336}
+    { 
+         "Asignatura" : "Física", 
+         "DineroaPagar" : 266 }
+    
+    /*Sabiendo que los trabajadores cobran dos euros menos la asignatura de lo que los alumnos pagan, 
+    cuales son las ganancias del mes para la academia que deja cada profesor por asignatura*/
+    
+    db.asignaturas.aggregate([
+        {$lookup:
+        {from:'alumnos',
+        localField: '_id',
+        foreignField: 'idas',
+        as: 'alumnos'}},
+        {$lookup:
+            {from:'profesores',
+            localField: '_id',
+            foreignField: '_idas',
+            as: 'profesores'}},
+        {$project:
+        {
+        _id: 0,
+        Profesor: '$profesores.Nombre',
+        Asignatura: '$Materia',
+        DineroaPagar: {$multiply:[ {$sum: '$alumnos.Horas_aPagar'}, {$subtract: ['$PrecioHora', 2]}]},
+        Ganancias: {$subtract: [{$multiply:[ {$sum: '$alumnos.Horas_aPagar'},'$PrecioHora']},
+         {$multiply:[ {$sum: '$alumnos.Horas_aPagar'}, {$subtract: ['$PrecioHora', 2]}]}]}
+     }},
+     {$sort:{Ganancias:-1}}
+    ])
+
+    { "Profesor" : [ "Encarna" ],
+    "Asignatura" : "Griego",
+    "DineroaPagar" : 378, 
+    "Ganancias" : 126 },
+
+    {
+         "Profesor" : [ "Luisa" ], 
+    "Asignatura" : "Inglés", 
+    "DineroaPagar" : 496, 
+    "Ganancias" : 124 }
+    { 
+        "Profesor" : [ "Isabel" ], 
+        "Asignatura" : "Biología", 
+        "DineroaPagar" : 342, 
+        "Ganancias" : 114 }
+    { 
+        "Profesor" : [ "Juan" ], 
+        "Asignatura" : "Química", 
+        "DineroaPagar" : 330, 
+        "Ganancias" : 110 }
+    {
+         "Profesor" : [ "Lidia" ], 
+         "Asignatura" : "Latín", 
+         "DineroaPagar" : 350, 
+         "Ganancias" : 100 }
+    {
+         "Profesor" : [ "Blanca" ], 
+         "Asignatura" : "Lengua", 
+         "DineroaPagar" : 329, 
+         "Ganancias" : 94 }
+    {
+         "Profesor" : [ "Natalia" ], 
+         "Asignatura" : "Francés", 
+         "DineroaPagar" : 423, 
+         "Ganancias" : 94 }
+    {
+         "Profesor" : [ "Edurado" ], 
+         "Asignatura" : "Matemáticas", 
+         "DineroaPagar" : 270, 
+         "Ganancias" : 90 }
+    {
+         "Profesor" : [ "Martina" ], 
+         "Asignatura" : "Tecnología", 
+         "DineroaPagar" : 336, 
+         "Ganancias" : 84 }
+    {
+         "Profesor" : [ "Alberto" ], 
+         "Asignatura" : "Física", 
+         "DineroaPagar" : 266, 
+         "Ganancias" : 76 }
+
+
+    /*Si un profesor imparte de 100 horas semanales tendran un plus de ganancias mayor que si no, 
+    de forma que; si tiene más de 100 horas, se le restará 100 y se multiplicará 0.8 al resultado,
+    en caso contrario las horas impartidas se multiplican por 0.02*/
+
+    db.asignaturas.aggregate([
+        {$lookup:
+        {from:'alumnos',
+        localField: '_id',
+        foreignField: 'idas',
+        as: 'alumnos'}},
+        {$lookup:
+            {from:'profesores',
+            localField: '_id',
+            foreignField: '_idas',
+            as: 'profesores'}},
+        {$project:
+        {
+        _id: 0,
+        Profesor: {$concat: ['$profesores.Nombre','','$profesores.Apellidos']},
+        Asignatura: '$Materia',
+        DineroaPagar: {$multiply:[ {$sum: '$alumnos.Horas_aPagar'}, {$subtract: ['$PrecioHora', 2]}]},
+        Ganancias: {$subtract: [{$multiply:[ {$sum: '$alumnos.Horas_aPagar'},'$PrecioHora']},
+         {$multiply:[ {$sum: '$alumnos.Horas_aPagar'}, {$subtract: ['$PrecioHora', 2]}]}]},
+        plus: {$cond: { if: {$gte: ['$Ganancias',110]}, then:[{$multiply: [{$subtract: ['$Ganancias',100]}, 0.8]}], else:[{$multiply: ['$Ganancias', 0.02]}]}}
+    }},
+      {$sort:{Ganancias:-1}}
+    ])
+
+    { 
+        "Profesor" : [ "Encarna Yanes Vega" ], 
+        "Asignatura" : "Griego", 
+        "DineroaPagar" : 378, 
+        "Ganancias" : 126, 
+        "plus": 20.8}
+    { 
+        "Profesor" : [ "Luisa Caballero Muñoz" ],
+         "Asignatura" : "Inglés", 
+         "DineroaPagar" : 496, 
+         "Ganancias" : 124, 
+         "plus": 19.2}
+    { 
+        "Profesor" : [ "Isabel Cuevas Jimenez" ],
+        "Asignatura" : "Biología", 
+        "DineroaPagar" : 342, 
+        "Ganancias" : 114, 
+        "plus": 11.2}
+    {
+         "Profesor" : [ "Juan Martín Rodríguez" ], 
+         "Asignatura" : "Química", 
+         "DineroaPagar" : 330, 
+         "Ganancias" : 110, 
+         "plus": 8}
+    {
+         "Profesor" : [ "Lidia Aguilar Montagout" ], 
+         "Asignatura" : "Latín", 
+         "DineroaPagar" : 350, 
+         "Ganancias" : 100, 
+         "plus": 0.8}
+    { 
+        "Profesor" : [ "Blanca Cordero Benitez" ], 
+        "Asignatura" : "Lengua", 
+        "DineroaPagar" : 329, 
+        "Ganancias" : 94, 
+        "plus": 1.88}
+    {
+         "Profesor" : [ "Natalia Diez Claro" ], 
+         "Asignatura" : "Francés", 
+         "DineroaPagar" : 423, 
+         "Ganancias" : 94, 
+         "plus": 1.88}
+    {
+         "Profesor" : [ "Edurado Juan Escalona" ], 
+         "Asignatura" : "Matemáticas", 
+         "DineroaPagar" : 270, 
+         "Ganancias" : 90, 
+         "plus": 1.8}
+    {
+         "Profesor" : [ "Martina Barcia Mejias" ], 
+         "Asignatura" : "Tecnología", 
+         "DineroaPagar" : 336, 
+         "Ganancias" : 84, 
+         "plus": 1.68}
+    {
+         "Profesor" : [ "Alberto García Álvaro" ], 
+         "Asignatura" : "Física", 
+         "DineroaPagar" : 266, 
+         "Ganancias" : 76, 
+         "plus": 1.52}
+
+
+/*Tenemos una tarifa plus para los alumnos que han impartido más de 200 clases, para ellos, 
+cada 10 clases, 1 es gratis.¿Cuántas clases deben este mes ralmente?*/
+    db.alumnos.aggregate([
+        { $match: {$expr: { $gt: [ "$Horas_asistidas", 200 ]}}},
+        {$group:
+            { _id:{ año: {$year: "$Fecha_inscripción"}}, AlumnosPlusporAño:{$sum: 1 }}},
+        {$lookup:
+            {from:'asignaturas',
+            localField: 'idas',
+            foreignField: '_id',
+            as: 'alumnos'}},
+        {$project:
+        {_id:1,
+        AlumnosPlusporAño:1,
+        Asignatura: '$Materia',
+        TarifaPlus: {$subtract: ['$Horas_aPagar',{$divide: ['$Horas_aPagar',10]}]}}}
+            
+    ])
+    { 
+        "_id" : [2017], 
+        "AlumnosPlusporAño" : 1 , 
+        "Asignatura" : "Biología", 
+        "TarifaPlus" : 18}
+    {
+         "_id" : [2019], 
+         "AlumnosPlusporAño" : 1 , 
+         "Asignatura" : "Física", 
+         "TarifaPlus" : 11}
+    {
+         "_id" : [2019], 
+         "AlumnosPlusporAño" : 1 , 
+         "Asignatura" : "Latín", 
+         "TarifaPlus" : 15}
---
+++ scr/inserciones.js
@@ -0,0 +1,231 @@
+db.alumnos.drop()
+
+/*Las horas a pagar son las realizadas en el último mes, que acaba de finalizar */ 
+db.alumnos.insertMany([
+    {
+        '_id': 01,
+    Nombre: 'Joaquín',
+    Apellidos: 'López González',
+    Edad: 19,
+    Curso: '2BACH',
+    Sexo: 'M',
+    Horas_asistidas: 80,
+    Horas_aPagar: 15, 
+    Fecha_inscripción: ISODate('2020-09-01'),
+    idas: 1
+},
+    {
+        '_id': 02, 
+    Nombre: 'Almudena', 
+    Apellidos: 'García Pérez',
+    Edad: 14, 
+    Curso: '2ESO', 
+    Sexo: 'F', 
+    Horas_asistidas: 231, 
+    Horas_aPagar: 26, 
+    Fecha_inscripción: ISODate('2018-11-30'), 
+    idas: 3
+},
+    {
+        '_id': 03, 
+        Nombre: 'Marta', 
+        Apellidos: 'Olivera Delgado', 
+        Edad: 13, 
+        Curso: '1ESO', 
+        Sexo: 'F', 
+        Horas_asistidas: 250, 
+        Horas_aPagar: 20, 
+        Fecha_inscripción: ISODate('2018-12-16'), 
+        idas: 5
+    },
+    { 
+        '_id': 04, 
+        Nombre: 'Juan', 
+        Apellidos: 'Rodríguez Bernal', 
+        Edad: 17, 
+        Curso: '1BACH', 
+        Sexo: 'M', 
+        Horas_asistidas: 50, 
+        Horas_aPagar: 16, 
+        Fecha_inscripción: ISODate('2020-10-28'), 
+        idas: 6
+    },
+    {
+        '_id': 05, 
+        Nombre: 'Mario', 
+        Apellidos: 'Jimenez Contreras', 
+        Edad: 16, 
+        Curso: '4ESO', 
+        Sexo: 'M', 
+        Horas_asistidas: 66, 
+        Horas_aPagar: 17, 
+        Fecha_inscripción: ISODate('2020-11-20'), 
+        idas: 2
+    },
+    {
+        '_id': 06, 
+        Nombre: 'Gema', 
+        Apellidos: 'León Navarro', 
+        Edad: 15, 
+        Curso: '3ESO', 
+        Sexo: 'F', 
+        Horas_asistidas: 109, 
+        Horas_aPagar: 14, 
+        Fecha_inscripción: ISODate('2020-07-19'), 
+        idas: 4
+    },
+    {
+        '_id': 07, 
+        Nombre: 'Jesús', 
+        Apellidos: 'Ramirez Álvarez', 
+        Edad: 17, 
+        Curso: '1BACH', 
+        Sexo: 'M', 
+        Horas_asistidas: 152, 
+        Horas_aPagar: 13, 
+        Fecha_inscripción: ISODate('2020-05-25'), 
+        idas: 7
+    },
+    {
+        '_id': 08, 
+        Nombre: 'Rafa', 
+        Apellidos: 'Romero Domínguez', 
+        Edad: 18, 
+        Curso: '1BACH', 
+        Sexo: 'M', 
+        Horas_asistidas: 200, 
+        Horas_aPagar: 12, 
+        Fecha_inscripción: ISODate('2019-02-06'), 
+        idas: 8
+    },
+    {
+        '_id': 09, 
+        Nombre: 'Rosario', 
+        Apellidos: 'De los Reyes Ruíz', 
+        Edad: 14, 
+        Curso: '1ESO', 
+        Sexo: 'F', 
+        Horas_asistidas: 35, 
+        Horas_aPagar: 26, 
+        Fecha_inscripción: ISODate('2020-12-15'), 
+        idas: 9
+    },
+    {
+        '_id': 10, 
+        Nombre: 'Laura', 
+        Apellidos: 'Sanchez Romero', 
+        Edad: 18, 
+        Curso: '2BACH', 
+        Sexo: 'F', 
+        Horas_asistidas: 38, 
+        Horas_aPagar: 24, 
+        Fecha_inscripción: ISODate('2021-02-15'), 
+        idas: 10},
+    {'_id': 11, Nombre: 'Javier', Apellidos: 'Benitez Hidalgo', Edad: 16, Curso: '3ESO', Sexo: 'M', Horas_asistidas: 45, Horas_aPagar: 16, Fecha_inscripción: ISODate('2021-02-20'), idas: 3},
+    {'_id': 12, Nombre: 'Raquel', Apellidos: 'Díaz de la Concha ', Edad: 17, Curso: '4ESO', Sexo: 'F', Horas_asistidas: 82, Horas_aPagar: 17, Fecha_inscripción: ISODate('2020-07-25'), idas: 4},
+    {'_id': 13, Nombre: 'Ana', Apellidos: 'Vázquez Rojas', Edad: 15, Curso: '3ESO', Sexo: 'F', Horas_asistidas: 220, Horas_aPagar: 19, Fecha_inscripción: ISODate('2018-12-14'), idas: 2},
+    {'_id': 14, Nombre: 'Julia', Apellidos: 'Ribas Morales', Edad: 14, Curso: '2ESO', Sexo: 'F', Horas_asistidas: 264, Horas_aPagar: 17, Fecha_inscripción: ISODate('2021-01-27'), idas: 1},
+    {'_id': 15, Nombre: 'Álvaro', Apellidos: 'Martín Martínez', Edad: 13, Curso: '1ESO', Sexo: 'M', Horas_asistidas: 248, Horas_aPagar: 19, Fecha_inscripción: ISODate('2017-02-12'), idas: 5},
+    {'_id': 16, Nombre: 'Luis', Apellidos: 'Duran Alcaide ', Edad: 15, Curso: '3ESO', Sexo: 'M', Horas_asistidas: 35, Horas_aPagar: 18, Fecha_inscripción: ISODate('2020-09-01'), idas: 6},
+    {'_id': 17, Nombre: 'Alejandro', Apellidos: 'Burgos Muñoz', Edad: 14, Curso: '2ESO', Sexo: 'M', Horas_asistidas: 26, Horas_aPagar: 16, Fecha_inscripción: ISODate('2020-09-01'), idas: 8},
+    {'_id': 18, Nombre: 'Sonia', Apellidos: 'Moreno Macia', Edad: 13, Curso: '1ESO', Sexo: 'F', Horas_asistidas: 109, Horas_aPagar: 15, Fecha_inscripción: ISODate('2020-09-01'), idas: 7},
+    {'_id': 19, Nombre: 'Silvia', Apellidos: 'Rincón Lavado', Edad: 19, Curso: '2BACH', Sexo: 'F', Horas_asistidas: 99, Horas_aPagar: 20, Fecha_inscripción: ISODate('2020-09-01'), idas: 9},
+    {'_id': 20, Nombre: 'Daniel', Apellidos: 'Romero Maldonado', Edad: 18, Curso: '2BACH', Sexo: 'M', Horas_asistidas: 15, Horas_aPagar: 10, Fecha_inscripción: ISODate('2020-09-01'), idas: 10},
+    {'_id': 21, Nombre: 'Lucía', Apellidos: 'Valero Ruíz', Edad: 17, Curso: '1BACH', Sexo: 'F', Horas_asistidas: 21, Horas_aPagar: 15, Fecha_inscripción: ISODate('2020-09-01'), idas: 1},
+    {'_id': 22, Nombre: 'Roberto', Apellidos: 'Risoto Barrero', Edad: 16, Curso: '4ESO', Sexo: 'M', Horas_asistidas: 23, Horas_aPagar: 19, Fecha_inscripción: ISODate('2020-09-01'), idas: 2},
+    {'_id': 23, Nombre: 'Carmen', Apellidos: 'Nogales González', Edad: 15, Curso: '2ESO', Sexo: 'F', Horas_asistidas: 26, Horas_aPagar: 20, Fecha_inscripción: ISODate('2020-09-01'), idas: 3},
+    {'_id': 24, Nombre: 'Jose', Apellidos: 'López Astola', Edad: 14, Curso: '2ESO', Sexo: 'M', Horas_asistidas: 23, Horas_aPagar: 18, Fecha_inscripción: ISODate('2020-09-01'), idas: 5},
+    {'_id': 25, Nombre: 'Paula', Apellidos: 'Pellin Bueno', Edad: 13, Curso: '1ESO', Sexo: 'F', Horas_asistidas: 17, Horas_aPagar: 14, Fecha_inscripción: ISODate('2020-09-01'), idas: 7},
+    {'_id': 26, Nombre: 'Pedro', Apellidos: 'Guerra Jurado', Edad: 18, Curso: '2BACH', Sexo: 'M', Horas_asistidas: 257, Horas_aPagar: 16, Fecha_inscripción: ISODate('2020-09-01'), idas: 6},
+    {'_id': 27, Nombre: 'Beatriz', Apellidos: 'Manzano Lobo', Edad: 17, Curso: '1BACH', Sexo: 'F', Horas_asistidas: 64, Horas_aPagar: 14, Fecha_inscripción: ISODate('2020-09-01'), idas: 4},
+    {'_id': 28, Nombre: 'Armando', Apellidos: 'Serrano Valencia', Edad: 16, Curso: '4ESO', Sexo: 'M', Horas_asistidas: 25, Horas_aPagar: 17, Fecha_inscripción: ISODate('2020-09-01'), idas: 9},
+    {'_id': 29, Nombre: 'Mónica', Apellidos: 'Toscano Moya', Edad: 15, Curso: '3ESO', Sexo: 'F', Horas_asistidas: 45, Horas_aPagar: 10, Fecha_inscripción: ISODate('2020-09-01'), idas: 8},
+    {'_id': 30, Nombre: 'Miguel', Apellidos: 'Dávila Durbán', Edad: 14, Curso: '1ESO', Sexo: 'M', Horas_asistidas: 21, Horas_aPagar: 13, Fecha_inscripción: ISODate('2020-09-01'), idas: 10}
+
+])
+
+    db.asignaturas.drop()
+    db.asignaturas.insertMany([
+    {
+        '_id': 1, 
+        Materia: 'Lengua', 
+        PrecioHora:9
+    },
+    {
+        '_id': 2, 
+        Materia: 'Química', 
+        PrecioHora:8
+    },
+    {
+        '_id': 3, 
+        Materia: 'Inglés', 
+        PrecioHora:10
+    },
+    {
+        '_id': 4, 
+        Materia: 'Matemáticas', 
+        PrecioHora:8
+    },
+    {
+        '_id': 5, 
+        Materia: 'Biología', 
+        PrecioHora:8
+    },
+    {'_id': 6, Materia: 'Latín', PrecioHora:9},
+    {'_id': 7, Materia: 'Tecnología', PrecioHora:10},
+    {'_id': 8, Materia: 'Física', PrecioHora:9},
+    {'_id': 9, Materia: 'Griego', PrecioHora:8},
+    {'_id': 10, Materia: 'Francés', PrecioHora:11},
+    ])
+
+    /* las horas de clases son las del último mes, es decir, las que se les debe*/
+
+    db.profesores.drop()
+    db.profesores.insertMany([
+        {
+            '_id': 1, 
+            Nombre: 'Juan', 
+            Apellidos: 'Martín Rodríguez', 
+            Edad: 37, 
+            Sexo: 'M', 
+            _idas: 2,
+        },
+        {
+            '_id': 2, 
+            Nombre: 'Blanca', 
+            Apellidos: 'Cordero Benitez', 
+            Edad: 35, 
+            Sexo: 'F',  
+            _idas: 1,
+        },
+        {
+            '_id': 3, 
+            Nombre: 'Luisa', 
+            Apellidos: 'Caballero Muñoz', 
+            Edad: 34, 
+            Sexo: 'F', 
+            _idas: 3,
+        },
+        {
+            '_id': 4, 
+            Nombre: 'Edurado', 
+            Apellidos: 'Juan Escalera', 
+            Edad: 46, 
+            Sexo: 'M',  
+            _idas: 4,
+        },
+        {
+            '_id': 5, 
+            Nombre: 'Isabel', 
+            Apellidos: 'Cuevas Jimenez', 
+            Edad: 36, 
+            Sexo: 'F', 
+            _idas: 5,
+        },
+        {'_id': 6, Nombre: 'Encarna', Apellidos: 'Yanes Vega', Edad: 56, Sexo: 'F', _idas: 9},
+        {'_id': 7, Nombre: 'Lidia', Apellidos: 'Aguilar Montagout', Edad: 41, Sexo: 'F', _idas: 6},
+        {'_id': 8, Nombre: 'Natalia', Apellidos: 'Diez Claro', Edad: 31, Sexo: 'F', _idas: 10},
+        {'_id': 9, Nombre: 'Alberto', Apellidos: 'García Álvaro', Edad: 49, Sexo: 'M', _idas: 8},
+        {'_id': 10, Nombre: 'Martina', Apellidos: 'Barcia Mejías', Edad: 32, Sexo: 'F', _idas: 7},
+    ])
+
